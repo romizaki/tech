dc3befffba9c08a0d9f75ad0c82edd5c099f5ba6
romizaki <rzferdiyanto@gmail.com>
2021-03-10 00:12:41 +0700

    first

---
+++ NodeJS/index.js
@@ -0,0 +1,12 @@
+function getPrimes(num) {
+  let array = [];
+  let prime;
+  for (let i = 2; i <= num - 1; i++) {
+    for (let j = 2; (prime = i === j || i % j !== 0) && j <= i / 2; j++) { }
+
+    prime && array.push(i);
+  }
+  return array;
+}
+console.log(getPrimes(5));
+console.log(getPrimes(9));
